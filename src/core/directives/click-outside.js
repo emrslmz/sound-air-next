98da8b0e6fae1f7e88f084492cf09e11652a024c
emrslmz <emrslmz57@gmail.com>
2022-07-15 12:13:36 +0300

    add comment line many files

--- src/core/directives/click-outside.js
+++ src/core/directives/click-outside.js
@@ -1,17 +1,17 @@
-/* eslint-disable */
-
-export default {
-  bind(el, binding, vnode) {
-    el.clickOutsideEvent = function (event) {
-      // here I check that click was outside the el and his children
-      if (!(el == event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', el.clickOutsideEvent);
-  },
-  unbind(el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent);
-  },
-};
+// /* eslint-disable */
+//
+// export default {
+//   bind(el, binding, vnode) {
+//     el.clickOutsideEvent = function (event) {
+//       // here I check that click was outside the el and his children
+//       if (!(el == event.target || el.contains(event.target))) {
+//         // and if it did, call method provided in attribute value
+//         vnode.context[binding.expression](event);
+//       }
+//     };
+//     document.body.addEventListener('click', el.clickOutsideEvent);
+//   },
+//   unbind(el) {
+//     document.body.removeEventListener('click', el.clickOutsideEvent);
+//   },
+// };
--- src/core/entitites/base.entity.ts
+++ src/core/entitites/base.entity.ts
@@ -1,9 +1,9 @@
-export default class BaseEntity {
-  private data: any;
-  private errors: any[];
-
-  constructor(data: any, errors: []) {
-    this.data = data;
-    this.errors = errors;
-  }
-}
+// export default class BaseEntity {
+//   private data: any;
+//   private errors: any[];
+//
+//   constructor(data: any, errors: []) {
+//     this.data = data;
+//     this.errors = errors;
+//   }
+// }
