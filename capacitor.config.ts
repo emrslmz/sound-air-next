97d72cbbdbf23801bad1e0b1d4af9d6875be6296
emrslmz <emrslmz57@gmail.com>
2022-08-11 19:22:04 +0300

    removed error codes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.soundairApp.android',
+  appName: 'sound_air_android_app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
